be1c55623a487c143701ae1a4fecaca4528c345c
mimal77777 <mimal77777@gmail.com>
2025-04-21 17:46:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,6 @@ def telegram_webhook():
     )
     return {"ok": True}
 
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
